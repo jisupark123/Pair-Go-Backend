92fcb4b3de2d3ccd28e2f22e380a76eb6340b65f
박지수 <jisupark2000@gmail.com>
2025-12-25 20:54:40 +0900

    Swagger 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  // Swagger setup
+  const { DocumentBuilder, SwaggerModule } = await import('@nestjs/swagger');
+  const config = new DocumentBuilder()
+    .setTitle('Pair Go API')
+    .setDescription('The Pair Go API description')
+    .setVersion('1.0')
+    .addCookieAuth('accessToken')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.use(cookieParser());
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
